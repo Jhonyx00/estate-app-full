06fabc91d482cd9fa7878c98c7be4fcc44eebc52
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-20 17:42:11 -0600

    add register function

--- controllers/auth.controler.ts
+++ controllers/auth.controler.ts
@@ -1,14 +1,30 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
-  //hash the password
 
-  const hashedpassword = await bcrypt.hash(password, 10);
+  try {
+    //hash the password
+    const hashedpassword = await bcrypt.hash(password, 10);
+    console.log(hashedpassword);
 
-  console.log(hashedpassword);
+    //create a new user and save to database
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedpassword,
+      },
+    });
 
-  //create a neew user and save to database
+    console.log(newUser);
+
+    res.status(201).json({ message: "User created succesfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
