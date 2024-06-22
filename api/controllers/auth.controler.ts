219cc937d4c54392cfcd4855ee7ebda061f669b6
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-21 22:27:05 -0600

    add logout function

--- api/controllers/auth.controler.ts
+++ api/controllers/auth.controler.ts
@@ -55,6 +55,8 @@ export const login = async (req, res) => {
       process.env.JWT_SECRET_KEY,
       { expiresIn: age }
     );
+
+    const { password: userPassword, ...userInfo } = user;
     res
       .cookie("token", token, {
         httpOnly: true,
@@ -62,7 +64,7 @@ export const login = async (req, res) => {
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Succesful" });
+      .json(userInfo);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to login" });
