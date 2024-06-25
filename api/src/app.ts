39abfd8739c91c5c4ad2c63be0ed9edb89c47fca
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-24 19:54:07 -0600

    add users endpoints

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,13 +5,15 @@ import cookieParser from "cookie-parser";
 import postRoute from "../routes/post.route";
 import authRoute from "../routes/auth.route";
 import testRoute from "../routes/test.route";
+import userRoute from "../routes/user.route";
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
+app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
